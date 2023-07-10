8d8fb9f4075769714ee4f04e6446d36cabb955a1
ADOwen <92840021+ADOwen@users.noreply.github.com>
2023-07-10 17:42:20 -0400

    Display Genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import { Grid, GridItem, Show } from '@chakra-ui/react';
-import NavBar from './components/NavBar';
-import GameGrid from './components/GameGrid';
-import GenreList from './components/GenreList';
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
-    <Grid templateAreas={{
-      base: `"nav" "main"`,
-      lg: `"nav nav" "aside main"` // 1024 px
-    }}>
-      <GridItem area='nav'>
-        <NavBar/>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024 px
+      }}
+      templateColumns={{
+        base: '1fr',
+        lg:'200px 1fr'
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
-      <Show above='lg'>
-        <GridItem area='aside'>
+      <Show above="lg">
+        <GridItem paddingX={5} area="aside">
           <GenreList />
         </GridItem>
       </Show>
-      <GridItem area='main'>
-        <GameGrid/>
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
     </Grid>
   );
-};
+}
 
 export default App;
